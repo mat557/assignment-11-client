a724c262accace496369ee36c6d302f6dacfa569
mat557 <matasnim11@gmail.com>
2022-05-03 15:56:35 +0600

    few Changes

--- src/component/Home/SingleServiceDetail/SingleServiceDetail.jsx
+++ src/component/Home/SingleServiceDetail/SingleServiceDetail.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Card } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 import useCustom from "../../../hook/useCustom";
@@ -7,34 +7,45 @@ import useCustom from "../../../hook/useCustom";
 
 const SingleServiceDetail = () => {
   const { idOfService } = useParams();
-  const [data] = useCustom();
-  const singleItem = data?.find((element) => element._id === +idOfService);
+
+  const [service,setService] = useState({});
+
+  useEffect(()=>{
+    const url = `http://localhost:5000/services/${idOfService}`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => setService(data))
+  },[])
+  
+  // const singleItem = data.find((element) => element._id === idOfService);
+  
   let result;
+  
   const handleDelivered = ()=>{
-    
-    console.log(result)
+    const item = (service.quantity);
+    result = item - 1;
   }
+
+
   return (
     <div className="w-75 mx-auto">
       <h5 className="text-center" >You have Choosed :</h5>
-      {singleItem ? (
+      
         <div className="w-50 mx-auto">
           <Card style={{ width: "18rem" }}>
-            <Card.Img variant="top" src={singleItem.img} />
+            <Card.Img variant="top" src={service.img} />
             <Card.Body>
-              <Card.Title>{singleItem.name}</Card.Title>
+              <Card.Title>{service.name}</Card.Title>
               <Card.Text>
-                {singleItem.description}
+                {service.description}
               </Card.Text>
-              <p>Price:{singleItem.price} & Quantity :{singleItem.quantity}</p>
-              <p>Provider:{singleItem.provider}</p>
-              <button onClick={() => handleDelivered} className="btn-style mx-auto d-block">Delivered</button>
+              <p>Price:{service.price} & Quantity :{result}</p>
+              <p>Provider:{service.provider}</p>
+              <button onClick={handleDelivered} className="btn-style mx-auto d-block">Delivered</button>
             </Card.Body>
           </Card>
         </div>
-      ) : (
-        <p>Loading...</p>
-      )}
+      
     </div>
   );
 };
--- src/hook/useCustom.js
+++ src/hook/useCustom.js
@@ -2,8 +2,10 @@ import { useEffect, useState } from "react"
 
 const useCustom = () =>{
     const [data,setData] = useState([]);
-     useEffect(()=>{
-         fetch(`${process.env.PUBLIC_URL}/productss.json`)
+    
+    // process.env.PUBLIC_URL}/productss.json
+    useEffect(()=>{
+         fetch(`http://localhost:5000/services`)
          .then(res => res.json())
          .then(data => setData(data))
      },[])
