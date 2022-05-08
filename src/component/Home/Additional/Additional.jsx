e400dfd6b30658445bbc3cba5c116a34aa01f469
mat557 <matasnim11@gmail.com>
2022-05-08 21:35:51 +0600

    additional part done

--- src/component/Home/Additional/Additional.jsx
+++ src/component/Home/Additional/Additional.jsx
@@ -1,13 +1,24 @@
-import React from 'react';
-import './Additional.css';
-
+import React from "react";
+import { Card } from "react-bootstrap";
+import auth from "../../../Firebase.init";
+import "./Additional.css";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 const Additional = () => {
-    return (
-        <div>
-            <h1>This is additional</h1>
-        </div>
-    );
+  const [user] = useAuthState(auth);
+  const userUrl = user?.photoURL;
+  return (
+    <div>
+        <h4 style={{color:"rgba(216, 71, 4, 0.894)"}} className="text-center" >We Are Keeping Those Information Of Yours</h4>
+        <p className="text-center">Login to check</p>
+      <Card className="w-50 mx-auto text-center">
+        <Card.Img className="d-flex align-items-center mx-auto" style={{height:"55px",width:"55px"}} src={userUrl} />
+        <Card.Body>
+          <Card.Title>{user?.displayName}</Card.Title>
+        </Card.Body>
+      </Card>
+    </div>
+  );
 };
 
 export default Additional;
